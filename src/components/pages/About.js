17f98be180d031f6c00ceae9694a4c28a7666990
Raul Alvarez <ra.lvarez11418@gmail.com>
2019-08-24 16:43:57 -0400

    added about page and router

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is a TodoList app v1.0.0. It's built in React</p>
+    </React.Fragment>
+  )
+}
